9913079ea9917a573511585ffc1c67912389dde6
Brendan Knapp <brendan.knapp@nps.edu>
2020-03-25 09:15:47 -0700

    le sigh

--- inst/include/libais.hpp
+++ inst/include/libais.hpp
@@ -1,4 +1,4 @@
 #ifndef MARITIME_LIBAIS_HPP
-#include "./libais/src/libais/ais.h"
-#include "./libais/src/libais/decode_body.h"
+#include "libais/src/libais/ais.h"
+#include "libais/src/libais/decode_body.h"
 #endif
