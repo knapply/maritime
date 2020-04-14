728cace5bb782f93780a57991cd44c9654612892
Brendan Knapp <brendan.knapp@nps.edu>
2020-04-14 08:45:50 -0700

    github actions

--- src/maritime/stream.hpp
+++ src/maritime/stream.hpp
@@ -907,11 +907,20 @@ inline void NMEA_Stream::push(NMEA&& nmea) {
 //
 //
 
+template<typename T>
+constexpr bool TRUE(const T&) {
+  return true;
+  
+}
+
+
 inline bool is_valid_simple(const Sentence& sentence) {
   return sentence.is_valid();
 }
-inline bool is_valid2(const Sentence& sentence) {
-  
+
+template <typename T, typename Predicate>
+inline bool is_valid(const Sentence& sentence) {
+  return sentence.is_valid() && Predicate(sentence);
 }
 
 //
